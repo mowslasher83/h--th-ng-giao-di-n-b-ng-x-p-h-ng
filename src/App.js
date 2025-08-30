27a929de4c60a709ea280cae07d6201e46635136
mowslasher83 <radeswaled@hotmail.com>
2025-08-30 00:49:57 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayerRankings from './PlayerRankings';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bảng Xếp Hạng Người Chơi</h1>
+      <PlayerRankings />
+    </div>
+  );
+}
+
+export default App;
